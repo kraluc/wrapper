e83e06036680d25549cf62ca947ac0c1801f5d7e
Vincent Ricci <vricci@cisco.com>
2023-05-05 11:41:04 -0700

    updated argparse

--- wrapper.py
+++ wrapper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-
 """
 wrapper.py
+
+
 """
 __title__ = "wrapper.py"
 __version__ = "0.0.1"
@@ -12,9 +13,12 @@ __maintainer__ = "Vincent Ricci"
 __email__ = "vricci@cisco.com"
 __status__ = "development"
 
+import os
 import logging
-import os,sys
 import argparse
+from pathlib import Path
+import sys
+
 
 # Define global variable logger
 logger = logging.getLogger(os.path.basename(__file__))
@@ -42,12 +46,12 @@ logger.addHandler(c_handler)
 logger.addHandler(f_handler)
 
 
-
-def main(input:str):
-    logger.debug("all good")
-    logger.info("start")
-    logger.warning("warning")
+def main(input: str, output: str, list: bool):
+    logger.debug("debug example")
+    logger.info("info example")
+    logger.warning("warning example")
     print(os.path.basename(__file__))
+
 
 if __name__ == "__main__":
     file = os.path.basename(__file__)
@@ -55,24 +59,8 @@ if __name__ == "__main__":
 
     # CLI argument Parsing
     parser = argparse.ArgumentParser(
-        prog="wrapper", description="wrap clis"
+        prog="wrapper", description="wrap commands read from file"
     )
-    # parser.add_argument(
-    #     "--archive",
-    #     "-a",
-    #     type=str,
-    #     default=TEST_ARCHIVE,
-    #     const=TEST_ARCHIVE,
-    #     nargs="?",
-    #     help="path to archive to decode. (Default: %r)" % TEST_ARCHIVE,
-    # )
-    # parser.add_argument(
-    #     "--clean",
-    #     "-c",
-    #     action="store_true",
-    #     default=False,
-    #     help="clean directory folder",
-    # )
     parser.add_argument(
         "--debug",
         "-d",
@@ -80,27 +68,30 @@ if __name__ == "__main__":
         default=False,
         help="enable debug",
     )
-    # parser.add_argument(
-    #     "--list",
-    #     "-l",
-    #     action="store_true",
-    #     default=False,
-    #     help="Select from list. (Default: False)",
-    # )
-    # parser.add_argument(
-    #     "--mode",
-    #     "-m",
-    #     choices=["w", "a"],
-    #     default="w",
-    #     help="syslog file mode ('w' or 'a'). (default: 'w')",
-    # )
-    # parser.add_argument(
-    #     "--released",
-    #     "-r",
-    #     action="store_true",
-    #     default=False,
-    #     help="list released decoders. (Default: False)",
-    # )
+    parser.add_argument(
+        "--list",
+        "-l",
+        action="store_true",
+        default=False,
+        help="list commands from input file. (Default: False)",
+    )
+    parser.add_argument(
+        "--mode",
+        "-m",
+        choices=["w", "a"],
+        default="w",
+        help="syslog file mode ('w' or 'a'). (default: 'w')",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        default="wrapped_clis.txt",
+        const="wrapped_clis.txt",
+        nargs="?",
+        help="target file where to save wrapped commands",
+    )
+
     parser.add_argument(
         "--input",
         "-i",
@@ -108,79 +99,49 @@ if __name__ == "__main__":
         default="clis.txt",
         const="clis.txt",
         nargs="?",
-        help="input file where commands are saved",
+        help="input files containing show commands",
     )
-    # parser.add_argument(
-    #     "--store",
-    #     "-o",
-    #     type=str,
-    #     default=DECODERS_STORE,
-    #     const=DECODERS_STORE,
-    #     nargs="?",
-    #     help="store containing all supported BU decoders. Use '-r' to list content (Default: %r)"
-    #     % DECODERS_STORE,
-    # )
-    # parser.add_argument(
-    #     "--table",
-    #     "-t",
-    #     type=str,
-    #     default=LATEST_DECODER_TABLE,
-    #     const=LATEST_DECODER_TABLE,
-    #     nargs="?",
-    #     help="decoder table. (Default: %r)" % LATEST_DECODER_TABLE,
-    # )
-    # parser.add_argument(
-    #     "--use-folder",
-    #     "-u",
-    #     action="store_true",
-    #     default=False,
-    #     help="Assign decoder from folder. (Default: False)",
-    # )
-    # parser.add_argument(
-    #     "--folder",
-    #     "-f",
-    #     type=str,
-    #     default=ACTIVE_DECODER_DIR,
-    #     const=ACTIVE_DECODER_DIR,
-    #     nargs="?",
-    #     help="folder containing active decoder and table. (Default: %r)"
-    #     % ACTIVE_DECODER_DIR,
-    # )
 
     args = parser.parse_args()
 
     try:
-
         if args.debug:
             logger.setLevel(logging.DEBUG)
         else:
             logger.setLevel(logging.INFO)
 
-        # # Clear log files as needed
-        # if args.mode == "w" and os.path.exists(LOG_FILE):
-        #     parent_dir = Path(LOG_FILE).parent
-        #     logs = [
-        #         os.path.join(parent_dir, f)
-        #         for f in os.listdir(parent_dir)
-        #         if ".log" in f
-        #     ]
-        #     print("\ndeleting logfile(s)\n")
-        #     for log in logs:
-        #         print("\t%r" % log)
-        #         with open(log, mode="w", encoding="UTF-8") as f:
-        #             f.truncate(0)
+        # Clear log files as needed
+        if args.mode == "w" and os.path.exists(LOG_FILE):
+            parent_dir = Path(LOG_FILE).parent
+            logs = [
+                os.path.join(parent_dir, f)
+                for f in os.listdir(parent_dir)
+                if ".log" in f
+            ]
+            print("\ndeleting logfile(s)\n")
+            for log in logs:
+                print("\t%r" % log)
+                with open(log, mode="w", encoding="UTF-8") as f:
+                    f.truncate(0)
 
         input = str(args.input)
-        # output = str(args.output)
+        output = str(args.output)
+        list = bool(args.list)
 
+        logger.info("--------- BEGIN %s -------", file)
         main(
             input,
+            output,
+            list,
         )
 
+    except ValueError as err:
+        logger.exception(err)
+        logger.warning("Value Error...exiting")
+        sys.exit()
     except KeyboardInterrupt as err:
         logger.exception(err)
         logger.warning("KeyboardInterrupt... exiting")
         sys.exit()
-
     finally:
         logger.info("-------- END %s --------", file)
